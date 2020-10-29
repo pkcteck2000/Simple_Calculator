13774da446e902f9523e5cb0ef102a13d163cdad
premrobosoft <premnath@robosoftin.com>
2020-10-29 20:16:25 +0530

    1) Calculator operations 2) Added Icons for Toggle button

---
+++ calculator_operations.js
@@ -0,0 +1,31 @@
+let result = "";
+
+const evaluate = () => {
+    try{
+        result = eval(result);
+        result = Number.isInteger(result)? eval(result) : eval(result).toFixed(4);
+    }
+    catch(error){
+        //result = error;
+        result = "Error"
+    }
+}
+
+const calculate = ( op ) => {
+    if(op === 'AC'){
+        result = "";
+    }
+    else if(op === '='){
+        evaluate();
+    }
+    else if(op === '+/-'){
+        result = result*-1;
+    }
+    else {
+        result += op;
+    }
+
+    document.getElementById('answer-text').innerHTML = result ? result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : 0;
+}
+
+document.getElementById('answer-text').innerHTML = 0;
--- light_to_dark_mode.js
+++ light_to_dark_mode.js
@@ -1,5 +1,7 @@
 const toggleMode = () => {
     document.getElementById('calculator-container').classList.toggle('bg-dark');
+    document.getElementsByClassName('fas')[0].classList.toggle('fa-sun');
+    document.getElementById('toggle-button').classList.toggle('toggle-button-dark');
 
     Object.values(document.getElementsByClassName('btn')).forEach(val => {
         val.classList.toggle('btn-dark');
